49a12ba0473f197cdd586b0ce034bcfd0d0daf3d
Paul Ashour <paul.ashour@gmail.com>
2018-10-10 14:31:47 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,24 @@
 def consolidate_cart(cart)
   # code here
+  # groups identical items
 end
 
 def apply_coupons(cart, coupons)
   # code here
+  # applies coupons by separating out the appropriate number of 
+  # target items 
 end
 
 def apply_clearance(cart)
   # code here
+  # applies 20% discout to all of the items on clearance
 end
 
 def checkout(cart, coupons)
   # code here
+  # call other methods and then calculate total cost
+  consolidate_cart(cart)
+  apply_coupons(cart, coupons)
+  apply_clearance(cart)
+  
 end
